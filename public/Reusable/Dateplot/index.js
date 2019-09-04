2cfdaed450d3055ead258ef3fb0b4efe1a59a5eb
Woo <leo9634@naver.com>
2019-09-04 17:01:31 -0400

    changed route to localhost directory - due to csv files configs

---
+++ public/Reusable/Dateplot/index.js
@@ -0,0 +1,24 @@
+
+
+let Dateplot = function() {
+
+
+    /************** converting string to dates **************/    
+    // let parseTime = d3.timeParse("%m/%d/%y");
+
+    // function rowConverter (d) {        //more like it ensures them to be date(not string) & integers(not string)
+    //     return {                            
+    //         Date: parseTime(d.Date),
+    //         Amount: parseInt(d.Amount)
+    //     }
+    // }
+    console.log('dateplot')
+    
+
+    
+    // console.log(dataset1,'dataset');
+    /************** converting string to dates **************/    
+}
+
+
+export default Dateplot;
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,29 @@
 
 import Scatterplot from "../Reusable/Scatterplot/index.js";
 import Bargraph from "../Reusable/Bargraph/index.js";
-
+// import Dateplot from "../Reusable/Dateplot/index.js"
 //import all modules 
 
 Scatterplot();
 Bargraph();
+// Dateplot();
+
+
+
+let dataset1;
+let parseTime = d3.timeParse("%m/%d/%y");
+
+function rowConverter (d) {        //more like it ensures them to be date(not string) & integers(not string)
+        return {                            
+            Date: parseTime(d.Date),
+            Amount: parseInt(d.Amount)
+        }
+}
+d3.csv("time_scale_data.csv", function(data) {
+    console.log(data,'see it?');
+})
+
+console.log('hm');
+    // .then(function(data) {
+    //     console.log(data);
+    // })
